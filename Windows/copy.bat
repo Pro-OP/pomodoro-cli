559dbd7fb6e507cd45ec0cf6b173c7a06c20b674
Bhanu Pratap Singh Bisht <52692924+TBPSB@users.noreply.github.com>
2019-10-31 20:29:45 +0530

    Update copy.bat

--- Windows/copy.bat
+++ Windows/copy.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %userprofile%
-cd downloads\windows
+cd downloads\pomodoro-cli-Feature-Adding_code_and_files\windows
  copy sound.mp3 c:\windows\system32
  copy setvol.exe c:\windows\system32
  
